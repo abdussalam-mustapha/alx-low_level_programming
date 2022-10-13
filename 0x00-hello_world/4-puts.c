c81a048c72a8b98e93d1dc7504b5623c1b84dfc3
abdussalam-mustapha <abdussalammustapha@gmail.com>
2022-10-13 09:59:21 -0700

    added task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  *
- *  * main - Entry point
+ * Description: A C program that prints with put function
  *
- *   *
+ * Return: Always 0 (Success)
  *
- *    * Description: A C program that prints with put function
- *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- *      */
+ */
 
 int main(void)
 {
